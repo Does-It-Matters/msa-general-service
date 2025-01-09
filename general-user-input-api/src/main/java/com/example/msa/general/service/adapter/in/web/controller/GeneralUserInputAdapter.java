5a4fc4c3058b55107b63943cb8bf9078b7226d55
glass <silver20190204@gmail.com>
2025-01-09 21:59:31 +0900

    동기화 테스트를 위한 컨트롤러 수정

--- general-user-input-api/src/main/java/com/example/msa/general/service/adapter/in/web/controller/GeneralUserInputAdapter.java
+++ general-user-input-api/src/main/java/com/example/msa/general/service/adapter/in/web/controller/GeneralUserInputAdapter.java
@@ -4,6 +4,21 @@ import com.example.msa.general.service.adapter.in.web.dto.request.GeneralSignUpR
 import com.example.msa.general.service.adapter.in.web.dto.response.GeneralSignUpResponse;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * <b> 일반 유저 입력 어댑터 </b>
+ * HTTP 요청을 받아 서비스 계층으로 전달하고 응답을 처리
+ */
 public interface GeneralUserInputAdapter {
+
+    // 회원가입 처리
     ResponseEntity<GeneralSignUpResponse> signUp(GeneralSignUpRequest body);
+
+    // 유저 정보 조회 처리
+//    ResponseEntity<GeneralUser> getUser(String id);
+
+    // 유저 정보 수정 처리
+    ResponseEntity<GeneralSignUpResponse> updateUser(String id, String newEmail, int newAge);
+
+    // 유저 정보 삭제 처리
+    ResponseEntity<GeneralSignUpResponse> deleteUser(String id);
 }
--- general-user-input-http/src/main/java/com/example/msa/general/service/adapter/in/web/controller/GeneralController.java
+++ general-user-input-http/src/main/java/com/example/msa/general/service/adapter/in/web/controller/GeneralController.java
@@ -3,13 +3,11 @@ package com.example.msa.general.service.adapter.in.web.controller;
 import com.example.msa.general.service.adapter.in.web.dto.request.GeneralSignUpRequest;
 import com.example.msa.general.service.adapter.in.web.dto.response.GeneralSignUpResponse;
 import com.example.msa.general.service.application.port.in.GeneralUserDataInputPort;
+import com.example.msa.general.service.application.port.in.GeneralUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <b> 일반 유저 컨트롤러 </b>
@@ -21,8 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class GeneralController implements GeneralUserInputAdapter {
+
     private final GeneralUserDataInputPort generalService;
 
+    /**
+     * <b> 회원가입 처리 </b>
+     *
+     * @param body 요청 데이터
+     * @return 회원가입 성공/실패 응답
+     */
     @PostMapping("/v1/general/sign-up")
     @Override
     public ResponseEntity<GeneralSignUpResponse> signUp(@RequestBody GeneralSignUpRequest body) {
@@ -33,5 +38,65 @@ public class GeneralController implements GeneralUserInputAdapter {
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new GeneralSignUpResponse("회원가입 실패"));
+    }
+
+    /**
+     * <b> 유저 정보 조회 </b>
+     *
+     * @param id 조회할 유저의 ID
+     * @return 유저 정보 또는 404 Not Found 응답
+     */
+    @GetMapping("/v1/general/{id}")
+//    @Override
+    public ResponseEntity<GeneralUser> getUser(@PathVariable String id) {
+        GeneralUser user = generalService.getUser(id);
+
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);  // 404 Not Found
+        }
+    }
+
+    /**
+     * <b> 유저 정보 수정 </b>
+     *
+     * @param id 수정할 유저의 ID
+     * @param newEmail 새로운 이메일
+     * @param newAge 새로운 나이
+     * @return 수정 성공/실패 응답
+     */
+    @PutMapping("/v1/general/{id}")
+    @Override
+    public ResponseEntity<GeneralSignUpResponse> updateUser(@PathVariable String id,
+                                                            @RequestParam String newEmail,
+                                                            @RequestParam int newAge) {
+        boolean updateSuccess = generalService.updateUser(id, newEmail, newAge);
+
+        if (updateSuccess) {
+            return ResponseEntity.ok(new GeneralSignUpResponse("유저 정보 수정 성공"));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new GeneralSignUpResponse("유저 정보 수정 실패"));
+        }
+    }
+
+    /**
+     * <b> 유저 정보 삭제 </b>
+     *
+     * @param id 삭제할 유저의 ID
+     * @return 삭제 성공/실패 응답
+     */
+    @DeleteMapping("/v1/general/{id}")
+    @Override
+    public ResponseEntity<GeneralSignUpResponse> deleteUser(@PathVariable String id) {
+        boolean deleteSuccess = generalService.deleteUser(id);
+
+        if (deleteSuccess) {
+            return ResponseEntity.ok(new GeneralSignUpResponse("유저 정보 삭제 성공"));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new GeneralSignUpResponse("유저 정보 삭제 실패"));
+        }
     }
 }
